21c17be727e73e54dd2261f3da9e32eb0669d259
Gabriel Staples <ercaguy@gmail.com>
2021-06-28 13:49:20 -0700

    bench_plot_results.py: wip: start adding subplots
    ...so I can have 2 plots on the figure: one for speed and one for memory
    usage.

--- bench_plot_results.py
+++ bench_plot_results.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Generates a figure that shows all benchmarking results
+"""Generates some figures (plots) that shows all benchmarking results
 """
 import sys
 import os
 import json
 
-import matplotlib.pyplot as plotlib
+import matplotlib.pyplot as plt
 
 filled_markers = ('o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X')
 colours = ('r', 'g', 'b', 'black', 'yellow', 'purple')
@@ -13,6 +13,7 @@ colours = ('r', 'g', 'b', 'black', 'yellow', 'purple')
 def plot_graphs(outfilename, benchmark_dict):
     """Plots the given dictionary of benchmark results
     """
+    # print("outfilename = {}".format(outfilename))
     # print("benchmark_dict = {}".format(benchmark_dict))
 
     # any one of the data sets--just to grab some subtitle data that is common to all the data sets
@@ -20,15 +21,28 @@ def plot_graphs(outfilename, benchmark_dict):
     data_dict = benchmark_dict[a_key][0]
     # print("data_dict = {}".format(data_dict))
 
-    plotlib.clf()
+    # Begin plot
+
+    plt.figure()
+    plt.clf()
     # main figure title
-    plotlib.suptitle("Malloc speed tests (tested w/glibc's `benchtests/bench-malloc-thread.c`)")
+    plt.suptitle("Malloc speed tests (tested w/glibc's `benchtests/bench-malloc-thread.c`)")
+
+    rows = 2
+    cols = 1
+
+    # Subplot 1
+
+    plt.subplot(rows, cols, 1)
+
     # figure subtitle
-    plotlib.title("num bytes per malloc: min = {}; max = {}".format(data_dict["min_size"],
-        data_dict["max_size"]), fontsize=10)
-    plotlib.xlabel('Number of threads')
+    plt.title("num bytes per malloc: min = {}; max = {}".format(data_dict["min_size"],
+        data_dict["max_size"]), fontsize=10, pad=30) # more `pad` shifts the subtitle UP more
+
+    plt.grid(alpha=.5) # alpha=0 is fully transparent; 1 is fully opaque
+    plt.xlabel('Number of threads')
     # bench-malloc-thread uses RDTSC counter for reporting time => CPU clock cycles
-    plotlib.ylabel('CPU cycles per sum of (1 free + 1 malloc) op')
+    plt.ylabel('CPU cycles per sum of (1 free + 1 malloc) op')
 
     nmarker=0
     max_x=[]
@@ -41,8 +55,8 @@ def plot_graphs(outfilename, benchmark_dict):
         X = [x["threads"] for x in data_list_of_dicts]
         Y = [y["time_per_iteration"] for y in data_list_of_dicts]
 
-        lines = plotlib.plot(X, Y, '-' + filled_markers[nmarker], label=impl_name)
-        plotlib.setp(lines, 'color', colours[nmarker])
+        lines = plt.plot(X, Y, '-' + filled_markers[nmarker], label=impl_name)
+        plt.setp(lines, 'color', colours[nmarker])
         
         # remember max X/Y
         # In case you only ran some of the tests, don't attempt to get `max()` on an empty list--ie:
@@ -54,9 +68,50 @@ def plot_graphs(outfilename, benchmark_dict):
         
         nmarker=nmarker+1
 
-    # set some graph global props:        
-    plotlib.xlim(0, max(max_x)*1.1)
-    plotlib.ylim(0, max(max_y)*1.3)
+    # set some graph global properties:
+    plt.xlim(0, max(max_x)*1.1)
+    plt.ylim(0, max(max_y)*1.3)
+    plt.legend(loc='upper left')
+
+    # Subplot 2
+
+    plt.subplot(rows, cols, 2)
+
+    plt.grid(alpha=.5)
+    plt.xlabel('Number of threads')
+    # bench-malloc-thread uses RDTSC counter for reporting time => CPU clock cycles
+    plt.ylabel('CPU cycles per sum of (1 free + 1 malloc) op')
+
+    nmarker=0
+    max_x=[]
+    max_y=[]
+    for impl_name in benchmark_dict.keys():
+        data_list_of_dicts = benchmark_dict[impl_name]
+        # print("data_list_of_dicts = {}".format(data_list_of_dicts))
+
+        # add a line plot
+        X = [x["threads"] for x in data_list_of_dicts]
+        Y = [y["time_per_iteration"] for y in data_list_of_dicts]
+
+        lines = plt.plot(X, Y, '-' + filled_markers[nmarker], label=impl_name)
+        plt.setp(lines, 'color', colours[nmarker])
+
+        # remember max X/Y
+        # In case you only ran some of the tests, don't attempt to get `max()` on an empty list--ie:
+        # for a benchmark you didn't run. Only operate if the lists aren't empty.
+        if X:
+            max_x.append(max(X))
+        if Y:
+            max_y.append(max(Y))
+
+        nmarker=nmarker+1
+
+    # set some graph global properties:
+    plt.xlim(0, max(max_x)*1.1)
+    plt.ylim(0, max(max_y)*1.3)
+    plt.legend(loc='upper left')
+
+    # Save and show plots
 
     outfilename_dir = os.path.dirname(outfilename)
     print("Writing plot into '%s'" % outfilename)
@@ -64,9 +119,10 @@ def plot_graphs(outfilename, benchmark_dict):
           "Close the plot to terminate the program. Run `RESULT_DIRNAME='{}' make plot_results`\n" +
           "to plot the results again.\n" +
           "- - -").format(outfilename_dir))
-    plotlib.legend(loc='upper left')
-    plotlib.savefig(outfilename)
-    plotlib.show()
+    figure = plt.gcf() # get current figure
+    figure.set_size_inches(8, 8)
+    plt.savefig(outfilename)
+    plt.show()
 
 
 def main(args):
